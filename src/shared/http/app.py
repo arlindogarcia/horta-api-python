108f189a2b7cddaab8b1bb6f6f3777a4a1b5bb21
Arlindo Garcia <arlindogarciagomesneto70@gmail.com>
2022-11-18 14:31:48 -0300

    Feat: adicionado cors

--- src/shared/http/app.py
+++ src/shared/http/app.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from src.config.database.connection import getConnection
+from flask_cors import CORS
 
 def getApp():
   app = Flask(__name__)
   app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
   app.config['SQLALCHEMY_DATABASE_URI'] = getConnection()
+  CORS(app)
   return app
