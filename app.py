ee287509ca5bc52475f7bf8f0855fad1fe647680
GABRIELZANUTO <gszanuto@outlook.com>
2022-12-12 12:33:08 -0300

    Reparo na função de pegar IPV4

--- app.py
+++ app.py
@@ -9,3 +9,4 @@ getRoutes(app, db)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
+    
--- src/config/http/system.py
+++ src/config/http/system.py
@@ -1,4 +1,4 @@
 import socket
 
 def getIpv4AddressLocal():
-  return socket.gethostbyname(socket.gethostname())
+  return socket.gethostbyname('Equipamento2') #Colocar o nome do computador que está rodando o container, para descobrir é só abrir o Terminal e rodar "HOSTNAME"
